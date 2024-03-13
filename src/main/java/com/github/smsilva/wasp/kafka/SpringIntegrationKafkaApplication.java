6082e2efaf6fc12de0fc18aaf02ad01af08e3c2d
Silvio Silva <smsilva@gmail.com>
2024-03-13 19:21:38 -0300

    feat: Spring initial files

---
+++ src/main/java/com/github/smsilva/wasp/kafka/SpringIntegrationKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.smsilva.wasp.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/smsilva/wasp/kafka/SpringIntegrationKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.smsilva.wasp.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
