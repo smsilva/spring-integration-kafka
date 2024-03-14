24001944fa0ccb15efd6c1257b9b250fc843b826
Silvio Silva <smsilva@gmail.com>
2024-03-14 11:20:48 -0300

    feat: Update channel type

--- src/main/java/com/github/smsilva/wasp/kafka/App.java
+++ src/main/java/com/github/smsilva/wasp/kafka/App.java
@@ -1,32 +1,28 @@
 package com.github.smsilva.wasp.kafka;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.channel.PublishSubscribeChannel;
 import org.springframework.integration.kafka.inbound.KafkaMessageDrivenChannelAdapter;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
-import org.springframework.messaging.PollableChannel;
+import org.springframework.messaging.SubscribableChannel;
 
 import static org.springframework.integration.kafka.inbound.KafkaMessageDrivenChannelAdapter.ListenerMode.record;
 
 @SpringBootApplication
 public class App {
 
-	private static final Logger log = LoggerFactory.getLogger(App.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 
 	@Bean
-	public PollableChannel kafkaInput() {
-		return new QueueChannel();
+	public SubscribableChannel kafkaInput() {
+		return new PublishSubscribeChannel();
 	}
 
 	@Bean
