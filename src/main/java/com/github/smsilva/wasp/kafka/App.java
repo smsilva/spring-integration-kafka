9865186d57ef9720dd639f62bddfb7233006a0b4
Silvio Silva <smsilva@gmail.com>
2024-03-13 23:28:30 -0300

    feat: Using Spring Integration

--- src/main/java/com/github/smsilva/wasp/kafka/App.java
+++ src/main/java/com/github/smsilva/wasp/kafka/App.java
@@ -2,11 +2,18 @@ package com.github.smsilva.wasp.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.kafka.inbound.KafkaMessageDrivenChannelAdapter;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.messaging.PollableChannel;
+
+import static org.springframework.integration.kafka.inbound.KafkaMessageDrivenChannelAdapter.ListenerMode.record;
 
 @SpringBootApplication
 public class App {
@@ -17,9 +24,28 @@ public class App {
 		SpringApplication.run(App.class, args);
 	}
 
-	@KafkaListener(id = "${spring.kafka.consumer.group-id}", topics = "quickstart-events")
-	public void listen(String in, @Header(KafkaHeaders.GROUP_ID) String groupId) {
-		log.info("Received: {} Group ID: {}", in, groupId);
+	@Bean
+	public PollableChannel kafkaInput() {
+		return new QueueChannel();
+	}
+
+	@Bean
+	public KafkaMessageDrivenChannelAdapter<String, String> kafkaMessageDrivenChannelAdapter(
+			KafkaMessageListenerContainer<String, String> container) {
+		KafkaMessageDrivenChannelAdapter<String, String> adapter =
+				new KafkaMessageDrivenChannelAdapter<>(container, record);
+		adapter.setOutputChannel(kafkaInput());
+		return adapter;
+	}
+
+	@Bean
+	public KafkaMessageListenerContainer<String, String> kafkaMessageListenerContainer(
+			ConsumerFactory<String, String> consumerFactory,
+			@Value("${spring.kafka.consumer.group-id}") String groupId) throws Exception {
+		ContainerProperties properties = new ContainerProperties("quickstart-events");
+		properties.setAckMode(ContainerProperties.AckMode.RECORD);
+		properties.setGroupId(groupId);
+		return new KafkaMessageListenerContainer<>(consumerFactory, properties);
 	}
 
 }
