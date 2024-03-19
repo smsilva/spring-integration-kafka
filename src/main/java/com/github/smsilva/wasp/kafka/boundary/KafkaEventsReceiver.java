6213ebc24d9afaa07c8ee4d5f8a0db8e85f2c06e
Silvio Silva <smsilva@gmail.com>
2024-03-19 11:33:49 -0300

    feat: Update Error Handling messages

--- src/main/java/com/github/smsilva/wasp/kafka/boundary/KafkaEventsReceiver.java
+++ src/main/java/com/github/smsilva/wasp/kafka/boundary/KafkaEventsReceiver.java
@@ -2,6 +2,7 @@ package com.github.smsilva.wasp.kafka.boundary;
 
 import com.github.smsilva.wasp.kafka.config.Channels;
 import com.github.smsilva.wasp.kafka.entity.Data;
+import com.github.smsilva.wasp.kafka.exceptions.BadData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.integration.annotation.ServiceActivator;
@@ -17,11 +18,18 @@ public class KafkaEventsReceiver {
 
     @ServiceActivator(inputChannel = Channels.EVENTS_INPUT)
     public void handle(Message<Data> message, @Headers KafkaMessageHeaders headers) {
+        Data payload = message.getPayload();
         log.info("Message: {}", message);
         log.info("Headers: {}", headers);
         log.info("Payload: {}", message.getPayload());
         log.info("  data.id: {}", message.getPayload().getId());
         log.info("  data.name: {}", message.getPayload().getName());
+
+        if (payload instanceof BadData badData) {
+            log.error("Bad data received: {}", badData);
+            log.error("  topic: {}", badData.getFailedDeserializationInfo().getTopic());
+            log.error("  exception.message: {}", badData.getFailedDeserializationInfo().getException().getMessage());
+        }
     }
 
 }
